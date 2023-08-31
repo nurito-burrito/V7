231afa12536fa8f889f0895a2f1b4d6dca6b5d30
Nurseda Balcioglu <nuritoburrito@Nursedas-MBP-2.home>
2023-08-31 16:43:56 +0100

    Add Sign in page field errors test

--- cypress/e2e/login_tests.cy.js
+++ cypress/e2e/login_tests.cy.js
@@ -1,17 +1,39 @@
 describe("Cypress Tests", () => {
-  it("Successful sign in", () => {
+  beforeEach(() => {
     cy.visit("/login");
     cy.get(".btn-provider-email").click();
-
     cy.url().should("include", "/email");
+  });
+
+  it("Successful sign in", () => {
     cy.get("#email").type(Cypress.env("mailslurpEmail"));
     cy.get("#password").type(Cypress.env("mailslurpDefaultPassword"));
-    cy.contains("Log In");
+    cy.contains("Log In").click();
 
     cy.url().should("include", "/organizations");
     cy.get("h2").contains("Test org").click();
 
     cy.url().should("include", "/projects");
     cy.get('[data-cy="organization-switcher-summary"]').should("be.visible");
+  });
+
+  it("Sign in page field errors", () => {
+    // email field
+    cy.get("#email").type("abc");
+    cy.contains("Log In").click();
+    cy.get("input:invalid").should("have.length", 1);
+    cy.get("#email").then(($input) => {
+      expect($input[0].validationMessage).to.contain(
+        "Please include an '@' in the email address."
+      );
+    });
+    cy.get("#email").type("@test.com");
+
+    // password field
+    cy.get("#password").type("123");
+    cy.contains("Log In").click();
+    cy.get(".error-message")
+      .should("be.visible")
+      .and("have.text", " Incorrect email address or password");
   });
 });
